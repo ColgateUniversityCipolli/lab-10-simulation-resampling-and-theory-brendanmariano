9cc6f72f6a6c67376757cf55ece3c24c136762ef
brendanmariano <bmariano@colgate.edu>
2025-04-04 16:36:41 -0400

    Need to check first raster plot and do last step

--- lab10script.R
+++ lab10script.R
@@ -18,7 +18,8 @@ hist = ggplot(data = dat) +
 
 (lower.val = quantile(dat$x, probs = .025))
 (upper.val = quantile(dat$x, probs = .975))
-(margin.of.error = (upper.val - lower.val)/2)
+(range = upper.val - lower.val)
+(margin.of.error = (range)/2)
 hist
 #################
 # Simulation (size 200)
@@ -33,7 +34,8 @@ hist2 = ggplot(data = dat2) +
 
 (lower.val2 = quantile(dat2$x, probs = .025))
 (upper.val2 = quantile(dat2$x, probs = .975))
-(margin.of.error2 = (upper.val2 - lower.val2)/2) 
+(range.2 = upper.val2-lower.val2)
+(margin.of.error2 = (range.2)/2) 
 hist2
 
 gallup.data = rep(1, times = 392)
@@ -57,9 +59,48 @@ hist.resample = ggplot() +
   geom_density(data = summarize.prop, aes(x = x))
 hist.resample
 
+(lower.val.rsample = quantile(summarize.prop$x, probs = .025))
+(upper.val.rsample = quantile(summarize.prop$x, probs = .975))
+(range.rsample = upper.val.rsample - lower.val.rsample)
+(margin.of.error.rsample = (range.rsample)/2) 
   print(summarize.prop)
 view(total.gallup.data)
 
-  
-  
+####################
+# Step 3 n and p
+####################
 
+probabilities.vector = seq(.01,.99,.01)
+n.p.data = tibble()
+for(j in 1:99)
+{
+  curr.vector = vector()
+  #Iterates through the different sizes
+  for(i in 0:290){
+    size.curr = 100 + (i)*10
+    curr.sim.data = rbinom(n = 10000, size = size.curr, prob = probabilities.vector[j])/size.curr
+    upper.sim = quantile(curr.sim.data, probs = .975)
+    lower.sim = quantile(curr.sim.data, probs = .025)
+    curr.vector = append(curr.vector, (upper.sim - lower.sim)/2)
+  }
+  curr.vector = tibble(moe = curr.vector) |>
+    bind_cols(tibble(size = seq(100,3000,10))) |>
+    bind_cols(tibble(probability = rep(probabilities.vector[j], 291)))
+  if(j == 1){
+    n.p.data = curr.vector
+  }
+  else{
+    n.p.data = bind_rows(n.p.data, curr.vector)
+  }
+}
+
+raster.plot = ggplot() + 
+  geom_raster(data = n.p.data, aes(x = size, y = probability, fill = moe)) +
+  scale_fill_viridis_c() + 
+  xlab("n") + 
+  ylab("Probability") + 
+  ggtitle("Margin Of Error With Respect To n And Probability") +
+  theme_bw()
+raster.plot
+view(n.p.data)
+print("d")
